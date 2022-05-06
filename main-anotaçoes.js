f5e535439e499f31e6e4ae091337e5adfebfefb7
Biamc <beatrizmcoutiho@yahoo.com.br>
2022-05-06 20:17:46 -0300

    cópia 3

--- main-anotaçoes.js
+++ main-anotaçoes.js
@@ -8,7 +8,23 @@ function onScroll(){
 onScroll()   //isso é como executar uma função
 //onscroll no html é a ação de rolar a pagina, ou seja quando rolar a pagina(executar essa ação) o 
 
+//String (textos)
+//Number (número)
+//Boolean (tue | false)
+// igual é os dois símbolos de "==""
+
 function onScroll(){
   navigation.classList.add('scroll')
   
 };
+
+function onScroll(){
+  if(scrollY > 0){
+  navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+};
+
+// if - se
+// else - se não
