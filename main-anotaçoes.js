72f791c9c65f09f8d3126b50ebb76c7b4801f6da
Biamc <beatrizmcoutiho@yahoo.com.br>
2022-05-05 23:09:54 -0300

    copia2

---
+++ main-anotaçoes.js
@@ -0,0 +1,14 @@
+//  alert('aqui')//
+// o alert é uma funçao, e o ('aqui') éum argumento de função
+
+function onScroll(){
+ 
+} //isso é a criaçõa de uma função
+
+onScroll()   //isso é como executar uma função
+//onscroll no html é a ação de rolar a pagina, ou seja quando rolar a pagina(executar essa ação) o 
+
+function onScroll(){
+  navigation.classList.add('scroll')
+  
+};
