dcb5cde224773a551790e4b1b672ba1cf384811c
Biamc <beatrizmcoutiho@yahoo.com.br>
2022-05-12 22:14:13 -0300

    Aula 3

--- main-anotaçoes.js
+++ main-anotaçoes.js
@@ -14,7 +14,7 @@ onScroll()   //isso é como executar uma função
 // igual é os dois símbolos de "==""
 
 function onScroll(){
-  navigation.classList.add('scroll')
+  navigation.classList.add('scroll') //DOM
   
 };
 
@@ -28,3 +28,15 @@ function onScroll(){
 
 // if - se
 // else - se não
+
+//  DOM é o document object model, que é todo o nosso código (objeto) HTML modelado para o javascript
+
+// todo ponto no js significa que o elemento anterior é um objeto
+
+ScrollReveal({
+  origin:'top'
+  distance: '30px'
+  duration: 700,
+}).reveal('#home');
+
+//origin top significa que a pagina vai se movimentar de cima para baixo
